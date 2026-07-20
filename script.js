c0290a93428e924a760b8083ae61f35fb8b92479
Jeyasurya R <jeyasurya953_email@gmail.com>
2026-07-20 21:21:52 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Task 1
+
+let name = "Naveen";
+let employeeId = "EMP1001";
+let department = "Frontend";
+let salary = 25000;
+let isPermanent = true;
+
+console.log("Name :", name);
+console.log("Employee ID :", employeeId);
+console.log("Department :", department);
+console.log("Salary :", salary);
+console.log("Permanent :", isPermanent);
