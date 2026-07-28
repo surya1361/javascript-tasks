0b7a631bef0df97aabbe422adcbee34927789200
Jeyasurya R <jeyasurya953_email@gmail.com>
2026-07-28 15:48:15 +0530

    Added Task-5 with all 20 JavaScript tasks

---
+++ assignment2/task5/script.js
@@ -0,0 +1,294 @@
+// ===============================
+// Task 1 - Student Registration
+// ===============================
+let studentName = "Jeyasurya";
+let department = "MCA";
+let cgpa = 8.6;
+
+console.log("----- Task 1 -----");
+console.log("Student Name :", studentName);
+console.log("Department :", department);
+console.log("CGPA :", cgpa);
+console.log("Status :", cgpa >= 7 ? "Eligible" : "Not Eligible");
+
+// ===============================
+// Task 2 - Online Shopping Bill
+// ===============================
+let productPrice = 2500;
+let quantity = 3;
+let total = productPrice * quantity;
+let discountAmount = total > 5000 ? total * 0.10 : 0;
+let finalPrice = total - discountAmount;
+
+console.log("\n----- Task 2 -----");
+console.log("Total :", total);
+console.log("Discount :", discountAmount);
+console.log("Final Price :", finalPrice);
+
+// ===============================
+// Task 3 - Employee Salary
+// ===============================
+let basicSalary = 35000;
+let bonus = 5000;
+let tax = 3000;
+let netSalary = basicSalary + bonus - tax;
+
+console.log("\n----- Task 3 -----");
+console.log("Basic Salary :", basicSalary);
+console.log("Bonus :", bonus);
+console.log("Tax :", tax);
+console.log("Net Salary :", netSalary);
+
+// ===============================
+// Task 4 - Movie Ticket Booking
+// ===============================
+let movie = "Jana Nayagan";
+let ticketPrice = 180;
+let tickets = 5;
+let totalAmount = ticketPrice * tickets;
+
+console.log("\n----- Task 4 -----");
+console.log("Movie :", movie);
+console.log("Tickets :", tickets);
+console.log("Total :", totalAmount);
+
+// ===============================
+// Task 5 - Login System
+// ===============================
+let username = "admin";
+let password = "12345";
+
+console.log("\n----- Task 5 -----");
+
+if (username === "admin" && password === "12345") {
+    console.log("Login Success");
+} else {
+    console.log("Invalid Username or Password");
+}
+
+// ===============================
+// Task 6 - Electricity Bill
+// ===============================
+let units = 250;
+let bill;
+
+if (units <= 100) {
+    bill = units * 5;
+} else if (units <= 200) {
+    bill = (100 * 5) + ((units - 100) * 7);
+} else {
+    bill = (100 * 5) + (100 * 7) + ((units - 200) * 10);
+}
+
+console.log("\n----- Task 6 -----");
+console.log("Units :", units);
+console.log("Bill :", bill);
+
+// ===============================
+// Task 7 - Restaurant Order
+// ===============================
+let menu = ["Dosa", "Idly", "Poori", "Meals"];
+
+console.log("\n----- Task 7 -----");
+console.log("First Item :", menu[0]);
+console.log("Last Item :", menu[menu.length - 1]);
+console.log("Total Items :", menu.length);
+
+menu.push("Fried Rice");
+menu.splice(menu.indexOf("Idly"), 1);
+
+console.log("Updated Menu :", menu);
+
+// ===============================
+// Task 8 - Employee Object
+// ===============================
+let employee = {
+    name: "Ravi",
+    role: "Frontend Developer",
+    salary: 40000
+};
+
+console.log("\n----- Task 8 -----");
+console.log("Employee Name :", employee.name);
+console.log("Role :", employee.role);
+console.log("Salary :", employee.salary);
+
+// ===============================
+// Task 9 - Product Inventory
+// ===============================
+let inventory = ["Mouse", "Keyboard", "Monitor", "CPU", "Speaker"];
+
+console.log("\n----- Task 9 -----");
+
+for (let i = 0; i < inventory.length; i++) {
+    console.log(i + 1, inventory[i]);
+}
+
+// ===============================
+// Task 10 - Function
+// ===============================
+function calculateArea(length, width) {
+    return length * width;
+}
+
+console.log("\n----- Task 10 -----");
+console.log("Area =", calculateArea(20, 10));
+
+// ===============================
+// Task 11 - Bank Deposit
+// ===============================
+let balance = 5000;
+
+function deposit(amount) {
+    balance += amount;
+}
+
+function withdraw(amount) {
+    balance -= amount;
+}
+
+deposit(2000);
+withdraw(1000);
+
+console.log("\n----- Task 11 -----");
+console.log("Final Balance :", balance);
+
+// ===============================
+// Task 12 - Callback Function
+// ===============================
+function addition(a, b) {
+    return a + b;
+}
+
+function calculate(callback, a, b) {
+    console.log(callback(a, b));
+    console.log("Calculation Completed");
+}
+
+console.log("\n----- Task 12 -----");
+calculate(addition, 10, 20);
+
+// ===============================
+// Task 13 - Generator Function
+// ===============================
+function* couponGenerator() {
+    yield "WELCOME10";
+    yield "SAVE20";
+    yield "SUPER30";
+    yield "BIG50";
+    yield "MEGA100";
+}
+
+console.log("\n----- Task 13 -----");
+
+let coupon = couponGenerator();
+
+console.log(coupon.next().value);
+console.log(coupon.next().value);
+console.log(coupon.next().value);
+console.log(coupon.next().value);
+console.log(coupon.next().value);
+
+// ===============================
+// Task 14 - Currying
+// ===============================
+function discount(a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c;
+        };
+    };
+}
+
+console.log("\n----- Task 14 -----");
+console.log(discount(10)(20)(30));
+
+// ===============================
+// Task 15 - Spread Operator
+// ===============================
+let frontend = ["HTML", "CSS", "JS"];
+let backend = ["Node", "Express", "MongoDB"];
+
+let fullstack = [...frontend, ...backend];
+
+console.log("\n----- Task 15 -----");
+console.log(fullstack);
+
+// ===============================
+// Task 16 - Rest Operator
+// ===============================
+function sum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+}
+
+console.log("\n----- Task 16 -----");
+console.log(sum(10, 20, 30, 40, 50));
+
+// ===============================
+// Task 17 - Array Destructuring
+// ===============================
+let colors = ["Red", "Blue", "Green", "Yellow"];
+
+let [c1, c2, c3, c4] = colors;
+
+console.log("\n----- Task 17 -----");
+console.log(c1);
+console.log(c2);
+console.log(c3);
+console.log(c4);
+
+// ===============================
+// Task 18 - Object Destructuring
+// ===============================
+let user = {
+    name: "Navi",
+    city: "Salem",
+    role: "Developer"
+};
+
+let { name, city, role } = user;
+
+console.log("\n----- Task 18 -----");
+console.log(name);
+console.log(city);
+console.log(role);
+
+// ===============================
+// Task 19 - Shopping Cart
+// ===============================
+let cart = ["Mobile", "Laptop", "Watch"];
+
+console.log("\n----- Task 19 -----");
+
+console.log("Original :", cart);
+
+cart.push("Headphone");
+console.log("After Push :", cart);
+
+cart.pop();
+console.log("After Pop :", cart);
+
+cart.shift();
+console.log("After Shift :", cart);
+
+cart.unshift("Tablet");
+console.log("After Unshift :", cart);
+
+// ===============================
+// Task 20 - Product Search
+// ===============================
+let products = [
+    "Laptop",
+    "Mouse",
+    "Keyboard",
+    "Monitor",
+    "Speaker"
+];
+
+console.log("\n----- Task 20 -----");
+
+console.log(
+    products.includes("Monitor")
+        ? "Product Available"
+        : "Out of Stock"
+);
